8902100ae72a4cc449510c01861787dae6bcf6b4
gingerknight <ryanrichmond6@gmail.com>
2025-07-03 22:13:38 -0600

    add clang-format

--- kilo.c
+++ kilo.c
@@ -13,29 +13,41 @@ struct termios orig_termios;
 
 /*** terminal ***/
 
-void die(const char *s) {
-    // perror() looks at the global errno variable and prints a descriptive error message
-    // and string given to provide context.
-    perror(s);
-    exit(1);
+void
+die (const char *s)
+{
+    // perror() looks at the global errno variable and prints a descriptive error
+    // message and string given to provide context.
+    perror (s);
+    exit (1);
 }
 
-void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
-        die("tcsetattr");
+void
+disableRawMode ()
+{
+    if (tcsetattr (STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die ("tcsetattr");
 }
 
-void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
-    atexit(disableRawMode);
+void
+enableRawMode ()
+{
+    tcgetattr (STDIN_FILENO, &orig_termios);
+    atexit (disableRawMode);
 
     struct termios raw = orig_termios;
     /*
-        * To disable a specific feature like ECHO (which causes typed characters to appear on the screen), we use a bitmask operation. ECHO is a bitflag with a value like 00000000000000000000000000001000 in binary. When we apply the bitwise-NOT operator (~ECHO), we get a mask where the ECHO bit is zero and all other bits are one: 11111111111111111111111111110111.
-        * By bitwise-ANDing this with the existing c_lflag, we clear only the ECHO bit, while leaving all other bits unchanged. This is a standard technique in C for disabling a specific flag.
-        * // ICANON - Canonical input (erase and kill processing).
-        * // ISIG - Enable signals
-    */
+     * To disable a specific feature like ECHO (which causes typed characters to
+     * appear on the screen), we use a bitmask operation. ECHO is a bitflag with a
+     * value like 00000000000000000000000000001000 in binary. When we apply the
+     * bitwise-NOT operator (~ECHO), we get a mask where the ECHO bit is zero and
+     * all other bits are one: 11111111111111111111111111110111. By bitwise-ANDing
+     * this with the existing c_lflag, we clear only the ECHO bit, while leaving
+     * all other bits unchanged. This is a standard technique in C for disabling a
+     * specific flag.
+     * // ICANON - Canonical input (erase and kill processing).
+     * // ISIG - Enable signals
+     */
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
@@ -43,25 +55,34 @@ void enableRawMode() {
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+    if (tcsetattr (STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+        die ("tcsetattr");
 }
 
 /*** init ***/
 
-int main() {
-    enableRawMode();
+int
+main ()
+{
+    enableRawMode ();
 
     char c;
-    while (1) {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
+    while (1)
+        {
+            char c = '\0';
+            if (read (STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+                die ("read");
+            if (iscntrl (c))
+                {
+                    printf ("%d\r\n", c);
+                }
+            else
+                {
+                    printf ("%d ('%c')\r\n", c, c);
+                }
+            if (c == 'q')
+                break;
         }
-        if (c == 'q') break;
-    }
 
     return 0;
 }
