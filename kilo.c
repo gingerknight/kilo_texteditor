d725ffc7b9890347f5a4d0aca74002008d4c423a
gingerknight <ryanrichmond6@gmail.com>
2025-07-03 21:00:56 -0600

    turn off ctirl+<letters> for control flow and other signals

--- kilo.c
+++ kilo.c
@@ -15,7 +15,14 @@ void enableRawMode() {
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO);
+    /*
+        * To disable a specific feature like ECHO (which causes typed characters to appear on the screen), we use a bitmask operation. ECHO is a bitflag with a value like 00000000000000000000000000001000 in binary. When we apply the bitwise-NOT operator (~ECHO), we get a mask where the ECHO bit is zero and all other bits are one: 11111111111111111111111111110111.
+        * By bitwise-ANDing this with the existing c_lflag, we clear only the ECHO bit, while leaving all other bits unchanged. This is a standard technique in C for disabling a specific flag.
+        * // ICANON - Canonical input (erase and kill processing).
+        * // ISIG - Enable signals
+    */
+    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
