be1ed2b1acc1c274750e29e4009d8bf6c61e419e
gingerknight <ryanrichmond6@gmail.com>
2025-07-04 17:18:16 -0600

    Dynamic window size, add cursor movement vim & arrow style

--- kilo.c
+++ kilo.c
@@ -8,6 +8,7 @@
 #include <errno.h>      // errno variable and error codes
 #include <stdio.h>      // printf(), perror()
 #include <stdlib.h>     // exit(), atexit()
+#include <string.h>     //memcpy()
 #include <sys/ioctl.h>  // TIOCGWINSZ (Terminal IOCtl Get WINdow SiZe)
 #include <termios.h>    // terminal I/O interfaces (tcgetattr(), tcsetattr())
 #include <unistd.h>     // read(), STDIN_FILENO
@@ -24,8 +25,8 @@
  *    0001 0001   = 17 = Ctrl-Q
  *
  */
-
 #define CTRL_KEY(letter) ((letter) & 0x1f)
+#define RYEDOC_VERSION "0.0.1"
 
 /*** data ***/
 
@@ -35,6 +36,7 @@
  * in raw mode after quitting the editor.
  */
 struct editorConfig {
+    int cx, cy;
     int screenrows;
     int screencols;
     struct termios orig_termios;
@@ -133,9 +135,50 @@ char editorReadKey() {
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
         if (nread == -1 && errno != EAGAIN) die("read");
     }
-    return c;
+
+    if (c == '\x1b') {
+        char seq[3];  // grab value after escape sequence
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+        if (seq[0] == '[') {
+            switch (seq[1]) {
+                case 'A':
+                    return 'j';
+                case 'B':
+                    return 'k';
+                case 'C':
+                    return 'l';
+                case 'D':
+                    return 'h';
+            }
+        }
+
+        return '\x1b';
+    } else {
+        return c;
+    }
 }
 
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return -1;
+}
 /*
  * Get window size col and row
  */
@@ -143,7 +186,9 @@ int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
 
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        return -1;
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+            return -1;  // https://vt100.net/docs/vt100-ug/chapter3.html#CUD
+        return getCursorPosition(rows, cols);
     } else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
@@ -151,33 +196,111 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+/*** append buffer ***/
+
+/*
+ * Replace write() calls with code that appends the string to a buffer, then writes
+ * Prevents flicker affect with multiple write calls for all the ~ and whatever we are typing
+ */
+
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+/*
+ * Allocate anough memory via realloc (grow current stack, or free and alloc new size)
+ * to hold current string, and what we are appending
+ * Use memcpy copy the string after end of current data in buffer then update *ptr and len
+ */
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+/*
+ * Free our struct after we are done, always clean your messes
+ */
+void abFree(struct abuf *ab) { free(ab->b); }
+
 /*** output ***/
 
 /*
  * Write column of ~ like vim
  */
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        if (y == E.screenrows / 3) {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome), "RyeRye editor --version %s", RYEDOC_VERSION);
+            if (welcomelen > E.screencols) welcomelen = E.screencols;
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding) {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while (padding--) abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        } else {
+            abAppend(ab, "~", 1);
+        }
+
+        abAppend(ab, "\x1b[K", 3);  // clear each line (erase in line)
+        if (y < E.screenrows - 1) {
+            abAppend(ab, "\r\n", 2);
+        }
     }
 }
 /*
  * write 4 bytes with escape sequence. Using the vt100 escape sequences.
  * The \x1b is escape character 27
  * https://vt100.net/docs/vt100-ug/chapter3.html#ED
- * <esc>[2J --> Erase all of the display – all lines are erased, changed to single-width, and the cursor does not move.
+ * <esc>[2J --> Erase all of the display – all lines are erased, changed to single-width, and the cursor does not
+ * move.
  * */
 void editorRefreshScreen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    struct abuf ab = ABUF_INIT;
 
-    editorDrawRows();
+    abAppend(&ab, "\x1[?25l", 6);  // hide cursor https://vt100.net/docs/vt510-rm/DECTCEM.html
+    abAppend(&ab, "\x1b[H", 3);
 
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    editorDrawRows(&ab);
+
+    char buf[32];
+    // move cursor to E.cx / E.cy
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+
+    abAppend(&ab, "\x1b[?25h", 6);  // cursor show
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
 }
 
 /*** input ***/
+
+void editorMoveCursor(char key) {
+    switch (key) {
+        case 'h':
+            E.cx--;
+            break;
+        case 'j':
+            E.cy--;
+            break;
+        case 'k':
+            E.cy++;
+            break;
+        case 'l':
+            E.cx++;
+            break;
+    }
+}
 
 void editorProcessKeypress() {
     char c = editorReadKey();
@@ -188,6 +311,13 @@ void editorProcessKeypress() {
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
+
+        case 'h':
+        case 'j':
+        case 'k':
+        case 'l':
+            editorMoveCursor(c);
+            break;
     }
 }
 
@@ -197,6 +327,9 @@ void editorProcessKeypress() {
  * This is to initilize all the fiedls in the E struct
  */
 void initEditor() {
+    E.cx = 0;
+    E.cy = 0;
+
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
